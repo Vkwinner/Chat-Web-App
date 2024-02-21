c20292fd435c1ecfd2f6fea606d6faf8de846415
Vkwinner <vkbedi25@gmail.com>
2024-02-21 15:23:42 +0530

    radio button

--- src/components/DropdownButton.jsx
+++ src/components/DropdownButton.jsx
@@ -9,23 +9,26 @@ import {
 const DropdownButton = (props) => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const toggleDropdown = () => setDropdownOpen(prevState => !prevState);
-  const [styles,setStyles] = useState({})
+  
 
   return (
     <div className="d-flex justify-content-start align-items-start">
       <Dropdown isOpen={dropdownOpen} toggle={toggleDropdown} dark flip>
-        <DropdownToggle caret size='md' className="d-flex align-items-center "style={{minWidth:"200px"}} >{props.initialValue} </DropdownToggle>
+        <DropdownToggle caret size='sm' className="d-flex align-items-center justify-content-center"style={{minWidth:"200px"}} >{props.initialValue}</DropdownToggle>
         <DropdownMenu>
           <DropdownItem onClick={() => {
             props.onOptionSelect(props.firstOption)
-            setStyles({styles:{"backgroundColor":"green"}})
             }}>
             {props.firstOption}
           </DropdownItem>
-          <DropdownItem onClick={() => props.onOptionSelect(props.secondOption)}>
-            {props.secondOption}
+          <DropdownItem onClick={() => {
+            props.onOptionSelect(props.secondOption)
+          }}>
+          {props.secondOption}
           </DropdownItem>
-          <DropdownItem onClick={() => props.onOptionSelect(props.thirdOption)}>
+          <DropdownItem onClick={() => {
+            props.onOptionSelect(props.thirdOption)
+          }}>
             {props.thirdOption}
           </DropdownItem>
         </DropdownMenu>
